a09603d49fdc496981f55d6bbb930f607ed41088
parkmawani <wlsgur7313@naver.com>
2023-01-03 11:00:42 +0900

    add gradlew

---
+++ src/test/java/com/parkmawani/starbucksproject/StarbucksprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkmawani.starbucksproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbucksprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
