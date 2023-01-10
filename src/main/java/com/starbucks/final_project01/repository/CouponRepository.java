b9ae3886ee48e0209121d53ce87a641ebbff9cf8
parkmawani <wlsgur7313@naver.com>
2023-01-10 14:48:50 +0900

    re

--- src/main/java/com/starbucks/final_project01/repository/CouponRepository.java
+++
@@ -1,9 +0,0 @@
-package com.starbucks.final_project01.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.starbucks.final_project01.entity.CouponEntity;
-
-public interface CouponRepository extends JpaRepository<CouponEntity, Long>{
-    
-}
--- src/main/java/com/starbucks/final_project01/store/vo/StoreMenuAddVO.java
+++
@@ -1,9 +0,0 @@
-package com.starbucks.final_project01.store.vo;
-
-import lombok.Data;
-
-@Data
-public class StoreMenuAddVO {
-    private Long store;
-    private Long menu;
-}
