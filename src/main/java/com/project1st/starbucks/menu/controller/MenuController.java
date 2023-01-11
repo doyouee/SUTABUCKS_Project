261c5764787826fb8ec616e12ce7fa2abd3f1e23
doyouee <aquariu92s@naver.com>
2023-01-11 15:11:58 +0900

    1차통합

--- src/main/java/com/project1st/starbucks/menu/controller/MenuController.java
+++ src/main/java/com/project1st/starbucks/menu/controller/MenuController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.project1st.starbucks.service.MenuService;
+import com.project1st.starbucks.admin.service.MenuService;
 
 
 
--- src/main/java/com/project1st/starbucks/store/controller/StoreController.java
+++ src/main/java/com/project1st/starbucks/store/controller/StoreController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.project1st.starbucks.service.StoreService;
+import com.project1st.starbucks.admin.service.StoreService;
 import com.project1st.starbucks.store.vo.StoreMenuAddVO;
 
 
