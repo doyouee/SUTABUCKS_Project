11c1dcaea9195bd70fb27c7c5c9cba5f89bc55ed
doyouee <aquariu92s@naver.com>
2023-01-25 15:42:37 +0900

    add membershipcard

--- src/main/java/com/project1st/starbucks/store/repository/StoreMenuConnectRepository.java
+++ src/main/java/com/project1st/starbucks/store/repository/StoreMenuConnectRepository.java
@@ -14,5 +14,4 @@ public interface StoreMenuConnectRepository extends JpaRepository<StoreMenuConne
     Page<StoreMenuConnectEntity> findByStore(StoreBasicInfoEntity store, Pageable pageable);
     StoreMenuConnectEntity findByStoreAndMenu(StoreBasicInfoEntity store, MenuBasicInfoEntity menu);  
     StoreMenuConnectEntity countByStore(StoreBasicInfoEntity store);
-
 }
