6b354cc4b2099c8f3405f2ab0237eb90292d47eb
doyouee <aquariu92s@naver.com>
2023-01-29 21:42:19 +0900

    20230129_이도영

--- src/main/java/com/project1st/starbucks/menu/service/MenuInfoService.java
+++ src/main/java/com/project1st/starbucks/menu/service/MenuInfoService.java
@@ -1,18 +1,37 @@
 package com.project1st.starbucks.menu.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import com.project1st.starbucks.admin.entity.MenuImageEntity;
 import com.project1st.starbucks.admin.entity.MenuNutritionEntity;
 import com.project1st.starbucks.admin.repository.MenuImageRepository;
@@ -23,6 +42,9 @@ import com.project1st.starbucks.menu.repository.MenuBasicInfoRepository;
 import com.project1st.starbucks.menu.repository.MenuQrRepository;
 import com.project1st.starbucks.menu.repository.ProductCategoryRepository;
 import com.project1st.starbucks.menu.vo.MenuStockVO;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 import com.project1st.starbucks.menu.vo.MenuDetailVO;
 import com.project1st.starbucks.menu.vo.MenuImageVO;
 
@@ -33,6 +55,7 @@ public class MenuInfoService {
     @Autowired MenuQrRepository menuQrRepo;
     @Autowired MenuImageRepository miRepo;
     @Autowired MenuNutritionRepository nutritionRepo;
+    @Value("${file.image.menuqr}") String qr_menu_img_path;
     
     // <전체 메뉴 조회하기> -> 완료 ♥
     public ResponseEntity<Object> menuList(Pageable pageable) {
@@ -114,4 +137,81 @@ public class MenuInfoService {
 
     }
 
+
+    // <메뉴의 QR코드 생성>
+    public ResponseEntity<Object> makeQR(String menuName) throws Exception {
+        Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
+        Long menuNo = mbiRepo.findByMbiName(menuName).getMbiSeq();
+        // String data = "http://www.naver.com";
+        // String data = "http://localhost:9999/menu/list";
+        // String data = "http://192.168.0.104:9999/menu/list";
+        String data = "http://haeji.mawani.kro.kr:9999/menu/list/detail?menuNo=" + menuNo;
+        String path = "D:\\home\\starbucks\\image\\menuqr\\" + menuName + ".jpg";
+        String charset = "UTF-8";
+        Map<EncodeHintType, ErrorCorrectionLevel> hashMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
+        hashMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+        createQR(data, path, charset, hashMap, 200, 200);
+
+        MenuQrEntity qrdata = MenuQrEntity.builder()
+            .mqiImageFile(menuName + ".jpg")
+            .mqiUri(menuName)
+            .mqiMbiSeq(menuNo).build();
+        qrdata = menuQrRepo.save(qrdata);
+
+        
+        resultMap.put("status", true);
+        resultMap.put("message", "성공적으로 QR코드를 만들었습니다.");
+        return new ResponseEntity<>(resultMap, HttpStatus.OK);
+    }
+
+
+    // <메뉴의 QR코드 생성 메서드>
+    public static void createQR(String data, String path, String charset, Map hashMap,  int height, int width) throws WriterException, IOException {
+        BitMatrix matrix = new MultiFormatWriter().encode(
+            new String(data.getBytes(charset), charset), BarcodeFormat.QR_CODE, width, height);
+        MatrixToImageWriter.writeToFile(matrix,path.substring(path.lastIndexOf('.') + 1),new File(path));
+    }
+
+
+    // <메뉴의 QR이미지 다운로드 메서드>
+    public ResponseEntity<Resource> getMenuQRImage (@PathVariable String uri, HttpServletRequest request) throws Exception {
+        String filename = null;
+        Path folderLocation = null;
+        
+        filename = getFilenameByUri(uri);
+        folderLocation = Paths.get(qr_menu_img_path);
+
+        String[] split = filename.split("\\.");
+        String ext = split[split.length - 1];
+        String exportName = uri + "." + ext; // 내보내줄 파일이름은 다르다. (내보낼 파일의 이름을 만든다.)                                                                                                                                                                                                                                                                  
+
+        Path targetFile = folderLocation.resolve(filename); //폴더 경로와 파일의 이름을 합쳐서 목표 파일의 경로를 만든다.
+        Resource r = null; //파일을 가져와서 resource라는 형태로 바꿔서 내보내줘야한다. ( 다운로드 가능한 형태로 변환하기 위한 Resurce 객체 생성 )
+        try {
+            r = new UrlResource(targetFile.toUri());
+        } catch (Exception e) { e.printStackTrace(); }
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(r.getFile().getAbsolutePath());
+            if(contentType == null) {
+                contentType = "application/octet-stream";
+            }
+        } catch (Exception e) { e.printStackTrace(); }
+        return ResponseEntity.ok()
+        .contentType(MediaType.parseMediaType(contentType))
+        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename*=\""+ URLEncoder.encode(exportName, "UTF-8") + "\"")
+        .body(r);
+    }
+
+    
+    // <파일명 가져오기>
+    public String getFilenameByUri(String uri) {
+        // List<MembershipCardQREntity> data = cardQRRepo.findTopByCardqrUriOrderByCardqrSeqDesc(uri);
+        return (menuQrRepo.findTopByMqiUriOrderByMqiSeqDesc(uri)).getMqiImageFile();
+        // return data.get(0).getCardqrUri();
+    }
+
+
+    
+    
 }
