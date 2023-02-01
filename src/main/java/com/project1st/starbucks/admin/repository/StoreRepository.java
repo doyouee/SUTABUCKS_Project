c6eae20ee134ff7027eccbd34d6ebfde4258e8bc
sjRyu <dhftlcm@gmail.com>
2023-02-01 17:24:59 +0900

    20230201수정

--- src/main/java/com/project1st/starbucks/admin/repository/StoreRepository.java
+++ src/main/java/com/project1st/starbucks/admin/repository/StoreRepository.java
@@ -10,4 +10,5 @@ import com.project1st.starbucks.admin.entity.StoreEntity;
 public interface StoreRepository extends JpaRepository<StoreEntity, Long>{
     public Integer countBySbiBranchName(String sbiBranchName);
     public Integer countBySbiSeq(Long sbiSeq);
+    public StoreEntity findBySbiBusinessAddress(String businessNum);
 }
